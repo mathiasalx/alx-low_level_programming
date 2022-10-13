d95dca948e6b4913a297470a1aed967913fa95a4
Mathias <mathias.alx1@gmail.com>
2022-10-13 04:49:56 -0700

    6. Size is not gradeur, and territory does not make a nation

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,5 +1,13 @@
 #include <stdio.h>
 
+/**
+ * main - Main function
+ *
+ * Description: Prints the size of various types
+ * on the computer it is compiled and run on
+ *
+ * Return: int
+ */
 int main(void)
 {
 	printf("Size of a char: %zu byte(s)\n", sizeof(char));
