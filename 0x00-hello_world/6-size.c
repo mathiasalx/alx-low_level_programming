93cb66337e7ff6085cb1bb8f59793681d5612794
Mathias <mathias.alx1@gmail.com>
2022-10-13 04:40:20 -0700

    6. Size is not gradeur, and territory does not make a nation

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -5,7 +5,7 @@ int main(void)
 	printf("Size of a char: %zu byte(s)\n", sizeof(char));
 	printf("Size of an int: %zu byte(s)\n", sizeof(int));
 	printf("Size of a long int: %zu byte(s)\n", sizeof(long int));
-	printf("Size of a long long int: %zu byte(s)\n", sizeof(unsigned long unsigned int));
+	printf("Size of a long long int: %zu byte(s)\n", sizeof(double unsigned int));
 	printf("Size of a float: %zu byte(s)\n", sizeof(float));
 
 	return (0);
