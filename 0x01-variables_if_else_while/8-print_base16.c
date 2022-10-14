93a4d2b0eb31345f099fcf316bedef841af21d47
Mathias <mathias.alx1@gmail.com>
2022-10-14 13:07:44 -0700

    8. Hexadecimal

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - Main function
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int i;
+	char a;
+
+	for (i = 0; i < 10; i++)
+	{
+		putchar(i + '0');
+	}
+
+	for (a = 'a'; a <= 'f'; a++)
+	{
+		putchar(a);
+	}
+
+	putchar('\n');
+
+	return (0);
+}
