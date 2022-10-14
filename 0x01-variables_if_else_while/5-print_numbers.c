6d13d262437e4fb3b2a6b8d921a1943ccdb5af0a
Mathias <mathias.alx1@gmail.com>
2022-10-14 12:11:16 -0700

    5. Numberss

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Main function
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	for (int i = 0; i < 10; i++)
+	{
+		printf("%d", i);
+	}
+
+	printf("\n");
+
+	return (0);
+}
