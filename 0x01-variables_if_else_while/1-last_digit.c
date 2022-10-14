9aac7aa3334fcc01d00cbfc275e6d10df5135f43
Mathias <mathias.alx1@gmail.com>
2022-10-14 10:19:36 -0700

    fix: 1. The last digit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -30,11 +30,6 @@ int main(void)
 	n = rand() - RAND_MAX / 2;
 	last_digit = n % 10;
 
-	if (last_digit < 0)
-	{
-		last_digit *= -1;
-	}
-
 	if (last_digit > 5)
 	{
 		print(n, last_digit, "greater than 5");
