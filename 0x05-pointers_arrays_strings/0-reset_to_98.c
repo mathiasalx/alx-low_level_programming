fbaa7140d336e93eb682a767249b2c05a1b8df2b
Mathias <mathias.alx1@gmail.com>
2022-10-24 08:32:48 -0700

    fix: style

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -3,7 +3,7 @@
 /**
  * reset_to_98 - takes a pointer to an int as parameter and updates the value
  * it points to to 98
- * @n: pointer to set to 98 
+ * @n: pointer to set to 98
  * Return: void
  */
 void reset_to_98(int *n)
